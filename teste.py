4736046fe13ee70a950b5c27738414b0ba660aa7
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-18 09:26:04 +0200

    recup max et min reseau

--- teste.py
+++ teste.py
@@ -4,8 +4,12 @@ import socket
 import ipaddress
 
 IP = input()
+tabipmask = IP.split("/")
+theIp = ipaddress.IPv4Address(tabipmask[0])
+net = ipaddress.IPv4Interface(IP)
+broad = ipaddress.IPv4Network(net.network,  False)
+print('IP: ', IP)
+print('Reseau: ', net.network)
+print('Broadcast: ', broad.broadcast_address)
 
-net = ipaddress.ip_address(IP)
-print('IP:', IP)
-print('Broadcast:', net.broadcast_address)
 
