b4fd8cf50730f885fdd3b47f1c1b90ee6acda162
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-18 19:27:56 +0200

    scan

--- teste.py
+++ teste.py
@@ -4,8 +4,8 @@ from scapy.all import ICMP, sr, IP, srloop, send
 
 
 def pingpong(theip):
-    sr(IP(dst=theip, src="192.168.236.0/24")/ICMP(), timeout=5)
+    sr(IP(dst=theip, src="192.168.236.8")/ICMP(), timeout=5)
     return
 
 
-pingpong("192.168.236.3")
+pingpong("192.168.236.0/24")
