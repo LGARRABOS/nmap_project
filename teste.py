fb1a5b17b78941813a41efcc29bb06e7574901c2
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-18 11:10:56 +0200

    test

--- teste.py
+++ teste.py
@@ -1,9 +1,8 @@
-#!C:\Users\etien\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe
 
-from scapy.all import ICMP, sr, IP
+from scapy.all import ICMP, sr, IP, srloop
 
-#conf.use_pcap = True
 
 IP = input()
+
 packet = sr(IP(dst=IP)/ICMP(), timeout=3)
 srloop(packet)
