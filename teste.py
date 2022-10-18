493fba33a8fd1ae90a909915ee00f6d33cac9675
LGARRABOS <luc.garrabos@ynov.com>
2022-10-18 11:11:28 +0200

    PingPong?

--- teste.py
+++ teste.py
@@ -1,6 +1,4 @@
-#!C:\Users\etien\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe
-
-from scapy.all import ICMP, sr, IP
+from scapy.all import ICMP, sr, IP, srloop
 
 #conf.use_pcap = True
 
