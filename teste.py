8ded3a6e73845245a12e3cbfa5d6523a1fac925b
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-18 11:27:54 +0200

    petite pause

--- teste.py
+++ teste.py
@@ -1,8 +1,12 @@
-
+# C:\Users\etien\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe
 from scapy.all import ICMP, sr, IP, srloop
 
 
-IP = input()
+ip = input()
 
-packet = sr(IP(dst=IP)/ICMP(), timeout=3)
-srloop(packet)
+def pingpong(theip):
+    ans, unans  = sr(IP(dst="10.3.2.0/24")/ICMP(), timeout=3)
+    srloop(ans, unans)
+    return 
+
+pingpong(ip)
