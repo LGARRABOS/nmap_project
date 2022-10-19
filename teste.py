4442d86b1f08db2f4003e2b18a18d42bb107ccb1
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-19 19:09:25 +0200

    test

--- teste.py
+++ teste.py
@@ -1,7 +1,10 @@
 # C:\Users\etien\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe
 from scapy.all import ICMP, sr, IP, srloop, send, arping
 
-reseauscan = input("Entrez le réseau que vous voulez scannez sous la forme 00.00.00.00/00")
+print("Entrez le réseau que vous voulez scannez sous la forme 00.00.00.00/00")
+reseauscan = input()
+
+
 
 def pingpong(theip):
 
