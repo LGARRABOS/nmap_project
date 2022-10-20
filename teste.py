ff5d8b664af36678736b7027d43d2c3e2ac93fa2
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-20 19:45:01 +0200

    fastping

--- teste.py
+++ teste.py
@@ -8,14 +8,17 @@ from datetime import datetime
 import json
 
 
-def pingpong(theip):
-    ans, unans = arping(theip)
+def pingpong(theip, scaninterface):
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp = ARP(pdst = theip)
+    answer, unanswered = srp(ether/arp, timeout = 2, iface = scaninterface, inter = 0.1)
+    # ans, unans = arping(theip)
     dict1 = {}
     compt = 0
     for sent, recieved in ans:
         # dict1[compt] = recieved
         # compt += 1
-        print(recieved)
+        print(recieved.summary())
     # out_file = open("resultscan.txt", "w")
     # print(json.dump(dict1, sort_keys=True, indent = 4))
     return ""
@@ -30,7 +33,7 @@ IpAddr = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['addr']
 Netmask = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['netmask']
 NetworkIP = ipaddress.ip_network(IpAddr + '/' + str(IPAddress(Netmask).netmask_bits()), strict=False)
 print(NetworkIP)
-print(pingpong(str(NetworkIP)))
+print(pingpong(str(NetworkIP), str(interfaces)))
 
 test.close()
 
