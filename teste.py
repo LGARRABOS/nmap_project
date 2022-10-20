4ed2387f2123d9a515fccffcdf2546392bc449d4
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-20 11:12:16 +0200

    test

--- teste.py
+++ teste.py
@@ -6,10 +6,7 @@ from scapy.all import *
 
 
 def pingpong(theip, myinterface):
-    arp = ARP(pdst=theip)
-    ether = Ether(dst = "ff:ff:ff:ff:ff:ff")
-    ans, unanswered = srp(ether / arp, timeout=2, iface=myinterface, inter=0.1)
-   # ans, unans = arping(theip)
+    ans, unans = arping(theip)
     for sent, recieved in ans:
         test.write(recieved.summary() + "\n")
     return ""
