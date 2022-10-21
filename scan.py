3683cd3e2df409d2691826e4664b36736f4fa8b8
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-21 09:23:57 +0200

    scan arp

--- scan.py
+++ scan.py
@@ -67,7 +67,7 @@ elif sys.argv[1] == "-a":
         new_scan = input()
         while(new_scan != "y" or new_scan != "n" ):
             print("A scan of this interface already exists. Do you want to make a new one?  y/n, ")
-            new_scan = input()
+            new_scan = "y"
         if(new_scan == "y"):
             IpReseauScan = startFirstScanping(scanInterface)
             file = open( scanInterface + ".json", "w")
