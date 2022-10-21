863b74f33cf3fd2877d0173765d7fb1b17697b4f
ECHALAYE <etienne.chalaye@ynov.com>
2022-10-21 10:59:49 +0200

    test

--- scan.py
+++ scan.py
@@ -55,18 +55,14 @@ def TCPPing(Ip):
     # ans, unans = sr( IP(dst=Ip)/TCP(dport=80, flags="S") )
     # for sent, recieved in ans:
     #     print(recieved.summary())
-    tabImportantPort = [20, 21, 22, 25, 35, 38, 57]
+    tabImportantPort = [20, 21, 22, 25, 35, 38, 80]
     for value in tabImportantPort:
         print(value)
         ans, unans = sr( IP(dst=Ip)/TCP(dport=value, flags="S") )
         for sent, recieved in ans:
             print(recieved.summary())
-    tabPort2 = [80, 143, 161, 162, 427, 548, 631]
-    for value in tabPort2:
-        print(value)
-        ans, unans = sr( IP(dst=Ip)/TCP(dport=value, flags="S") )
-        for sent, recieved in ans:
-            print(recieved.summary())
+
+
 
     
 
